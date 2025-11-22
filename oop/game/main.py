6f444494f927d561c77fcee1c3d51b7734197df3
Abhisek Bhujel <bhujelabhisek0@gmail.com>
2025-11-22 09:29:21 -0500

    inheritance update

--- oop/game/main.py
+++ oop/game/main.py
@@ -1,14 +1,28 @@
 from player import Player
+from enemy import Enemy, Troll, Vampire
 
 tim = Player("Tim")
 
-print(tim.name)
-print(tim.lives)
+random_enemy = Enemy("Basic enemy", 12, 1)
+print(random_enemy)
 
-print(tim)
+random_enemy.take_damage(4)
+print(random_enemy)
+
+random_enemy.take_damage(10)
+print(random_enemy)
+
+ugly_troll = Troll("Ugly_Troll")
+print(ugly_troll)
+
+ugly_troll.stomp()
+
+dracula = Vampire('dracula')
+print(dracula)
+
+dracula.take_damage(5)
+print(dracula)
 
 
-print(tim.score)
-tim.level = 2
-print(tim)
+
 
